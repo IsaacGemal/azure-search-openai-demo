e9b1ffb37a6c3383ae17f9116c1ead307d9ec4d7
Isaac <147355120+IsaacGemal@users.noreply.github.com>
2024-02-11 22:05:45 -0500

    Backtrack variable name changes

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -8,7 +8,7 @@ const DEFAULT_EXAMPLES: string[] = [
     "What are the ADA requirements for accessible routes within public spaces?"
 ];
 
-const ADA_DESIGN_EXAMPLES: string[] = [
+const GPT4V_EXAMPLES: string[] = [
     "Explain the differences between Type A and Type B accessible dwelling units under the ADA.",
     "How do ADA standards apply to digital accessibility on websites and mobile applications?",
     "Can you outline the ADA guidelines for visual alarms in public accommodations?"
@@ -16,13 +16,13 @@ const ADA_DESIGN_EXAMPLES: string[] = [
 
 interface Props {
     onExampleClicked: (value: string) => void;
-    useADADesign?: boolean;
+    useGPT4V?: boolean;
 }
 
-export const ExampleList = ({ onExampleClicked, useADADesign }: Props) => {
+export const ExampleList = ({ onExampleClicked, useGPT4V }: Props) => {
     return (
         <ul className={styles.examplesNavList}>
-            {(useADADesign ? ADA_DESIGN_EXAMPLES : DEFAULT_EXAMPLES).map((question, i) => (
+            {(useGPT4V ? GPT4V_EXAMPLES : DEFAULT_EXAMPLES).map((question, i) => (
                 <li key={i}>
                     <Example text={question} value={question} onClick={onExampleClicked} />
                 </li>
