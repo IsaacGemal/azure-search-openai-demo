a74df6144d74827c03f55d0a3798cd10512234b7
Isaac <147355120+IsaacGemal@users.noreply.github.com>
2024-02-11 21:21:44 -0500

    ADA addtion, frontend changes

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,26 +3,26 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "What are the minimum width requirements for doorways in ADA-compliant buildings?",
+    "How should parking spaces be designed to meet ADA standards?",
+    "What are the ADA requirements for accessible routes within public spaces?"
 ];
 
-const GPT4V_EXAMPLES: string[] = [
-    "Compare the impact of interest rates and GDP in financial markets.",
-    "What is the expected trend for the S&P 500 index over the next five years? Compare it to the past S&P 500 performance",
-    "Can you identify any correlation between oil prices and stock market trends?"
+const ADA_DESIGN_EXAMPLES: string[] = [
+    "Explain the differences between Type A and Type B accessible dwelling units under the ADA.",
+    "How do ADA standards apply to digital accessibility on websites and mobile applications?",
+    "Can you outline the ADA guidelines for visual alarms in public accommodations?"
 ];
 
 interface Props {
     onExampleClicked: (value: string) => void;
-    useGPT4V?: boolean;
+    useADADesign?: boolean;
 }
 
-export const ExampleList = ({ onExampleClicked, useGPT4V }: Props) => {
+export const ExampleList = ({ onExampleClicked, useADADesign }: Props) => {
     return (
         <ul className={styles.examplesNavList}>
-            {(useGPT4V ? GPT4V_EXAMPLES : DEFAULT_EXAMPLES).map((question, i) => (
+            {(useADADesign ? ADA_DESIGN_EXAMPLES : DEFAULT_EXAMPLES).map((question, i) => (
                 <li key={i}>
                     <Example text={question} value={question} onClick={onExampleClicked} />
                 </li>
--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>GPT + 2010 ADA Standards for Accessible Design</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
